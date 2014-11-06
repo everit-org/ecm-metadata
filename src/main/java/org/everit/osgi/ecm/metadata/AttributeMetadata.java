faae086b7a8e5779c36d8a8a52cc2c5addd9a7ca
balazs.zsoldos <balazs.zsoldos@everit.biz>
2014-11-06 11:16:34 +0100

    Refactoring

--- src/main/java/org/everit/osgi/ecm/metadata/AttributeMetadata.java
+++ src/main/java/org/everit/osgi/ecm/metadata/AttributeMetadata.java
@@ -1,18 +1,18 @@
 /**
- * This file is part of Everit - Component Metadata.
+ * This file is part of Everit - ECM Metadata.
  *
- * Everit - Component Metadata is free software: you can redistribute it and/or modify
+ * Everit - ECM Metadata is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Everit - Component Metadata is distributed in the hope that it will be useful,
+ * Everit - ECM Metadata is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Everit - Component Metadata.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Everit - ECM Metadata.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.everit.osgi.ecm.metadata;
 
